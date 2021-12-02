7ce9e517697e7f002bb727fc9d15f83a51b4c447
bikesh rai <bikeshrai78@gmail.com>
2021-12-02 21:16:32 +0545

    Display content of gameboard array

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+let game = (function(){
 const Gameboard = function(cellName,value){
     return {cellName,value};
 };
@@ -5,3 +6,25 @@ let gameboard = [];
 for(let i =1; i <= 9;i++){
     gameboard.push(Gameboard('cell' + i, ''));
 }
+
+gameboard[0].value = 'x';
+gameboard[5].value = 'o';
+
+//display user's input
+const displayGame = function(){
+    let cellValues = gameboard.filter(cellValue => cellValue.value != '');
+    cellValues.forEach(cell => {
+        let cellId = cell.cellName;
+        console.log(cellId);
+        document.querySelector(`#${cellId}`).textContent = cell.value;
+    });
+
+    console.log(cellValues);
+
+};
+return {
+    displayGame:displayGame
+};
+})();
+
+game.displayGame();

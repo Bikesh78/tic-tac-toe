09c2d79728c440c5f5218b33cc70baea97ba8208
bikesh rai <bikeshrai78@gmail.com>
2021-12-05 13:29:26 +0545

    Add model box to display result

--- JS/script.js
+++ JS/script.js
@@ -16,14 +16,18 @@ const PlayerTwo = function(boardIndex){
     return {gameboard};
 }
 
-
+let ratio = 0;
+let gameStatus = 'not over';
 let counter = 1;
 const playGame = function(){
     let clickedGrid = document.querySelector('#grid');
     clickedCell = clickedGrid.childNodes;
     clickedCell.forEach(clicked => {
         clicked.addEventListener('click',(e) =>{
-            clickedCellId = e.target.getAttribute('id'); //get the id of the clicked div
+            if(gameStatus == 'not over'){
+                clickedCellId = e.target.getAttribute('id'); //get the id of the clicked div
+            }
+            
             gameboardIndex = gameboard.findIndex(gameboard => gameboard.cellName === clickedCellId); //find index nuber of the array whose cellName matches with the id name of the div
             
             //only accepts input when the cell is empty
@@ -40,10 +44,12 @@ const playGame = function(){
                     checkGameState();
                 }
                 if(counter > 9){
-                    alert('Game is tied');
+                    displayResult('draw');
                 }
             }            
         });
+        
+        
         
     });
 }
@@ -57,7 +63,7 @@ const checkGameState = () =>{
        ||  gameboard[2].value === 'X' && gameboard[4].value === 'X' && gameboard[6].value === 'X'
        ||  gameboard[0].value === 'X' && gameboard[4].value === 'X' && gameboard[8].value === 'X'
         ){
-        alert('Player One Won');
+        displayResult('playerOne');
     } else if(gameboard[0].value === 'O' && gameboard[1].value === 'O' && gameboard[2].value === 'O'
     ||  gameboard[3].value === 'O' && gameboard[4].value === 'O' && gameboard[5].value === 'O'
     ||  gameboard[6].value === 'O' && gameboard[7].value === 'O' && gameboard[8].value === 'O'
@@ -67,7 +73,7 @@ const checkGameState = () =>{
     ||  gameboard[2].value === 'O' && gameboard[4].value === 'O' && gameboard[6].value === 'O'
     ||  gameboard[0].value === 'O' && gameboard[4].value === 'O' && gameboard[8].value === 'O'
      ){
-     alert('Player Two Won');
+     displayResult('playerTwo');
  }
 };
 //display user's input
@@ -77,6 +83,28 @@ const displayGame = function(){
         let cellId = cell.cellName;
         document.querySelector(`#${cellId}`).textContent = cell.value;
     });
+};
+
+const displayResult = (winner) =>{
+    gameStatus = 'over';
+    let resultMsgBox = document.createElement('div');
+    resultMsgBox.setAttribute('class','game-result');
+    resultMsgBox.style.display = 'flex';
+    let resultMsg = document.createElement('p');
+    resultMsgBox.appendChild(resultMsg);
+    if(winner === 'playerOne'){
+        resultMsg.textContent = 'The Winner Is Player One';
+    }else if(winner === 'playerTwo'){
+        resultMsg.textContent = 'The Winner Is Player Two';
+    }else{
+        resultMsg.textContent = 'It\'s A Tie';
+    }
+    document.querySelector('#container').appendChild(resultMsgBox);
+    let playAgainBtn = document.createElement('button');
+    playAgainBtn.setAttribute('class','button play-again');
+    playAgainBtn.textContent = 'Play Again'
+    resultMsgBox.appendChild(playAgainBtn);
+    
 };
 
 return {
